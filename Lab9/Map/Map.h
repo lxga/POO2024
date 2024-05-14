bb0ef46beffb71cd28dd2909475b4173711511d8
Luca <luca43gavrilas@gmail.com>
2024-05-14 12:45:57 +0300

    Lab9

---
+++ Lab9/Map/Map.h
@@ -0,0 +1,21 @@
+template<class K,class V>
+class Map {
+	void Set(const K& key, V& value) {
+
+	}
+	bool Get(const K& key, V& value) {
+
+	}
+	int Count() {
+
+	}
+	void Clear() {
+
+	}
+	bool Delete(const K& key) {
+
+	}
+	bool Includes(const Map<K, V>& map) {
+
+	}
+};
